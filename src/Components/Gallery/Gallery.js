14cb445cb6b07fd1f43da81b9e9f7f9529443763
Yashwitha <yashwitha@juegostudioz.com>
2022-10-31 17:15:23 +0530

    FEAT:Scrolling image container UI + styling

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,13 +1,16 @@
 import React from "react";
 import DropDown from "../DropDown/DropDown";
 import ImageContainer from "../ImageContainer/ImageContainer";
+import ScrollingImageContainer from "../ScrollingImageContainer/ScrollingImageContainer";
 import styles from "./Gallery.module.css";
+import "../../styles/global.css";
 
 function Gallery() {
   return (
     <div className={styles.gallery_conatiner}>
       <DropDown />
       <ImageContainer />
+      <ScrollingImageContainer />
     </div>
   );
 }
--- src/Components/ScrollingImageContainer/ScrollingImageContainer.js
+++ src/Components/ScrollingImageContainer/ScrollingImageContainer.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { enabled, title } from "../../utils/constants";
 import Button from "../Button/Button";
-import styles from "./ImageContainer.module.css";
+import styles from "./ScrollingImageContainer.module.css";
 
-function ImageContainer({ imageConatiner }) {
+function ScrollingImageContainer() {
   return (
     <div className="image_container">
       <Button title={title.previousBtnTitle}></Button>
-      <div
-        className={
-          imageConatiner ? imageConatiner : styles.main_image_conatiner
-        }
-      ></div>
+      <div className={styles.scrolling_image_container}>
+        <div className="styles.slides"></div>
+      </div>
       <Button title={title.nextBtnTitle} style={enabled}></Button>
     </div>
   );
 }
 
-export default ImageContainer;
+export default ScrollingImageContainer;
