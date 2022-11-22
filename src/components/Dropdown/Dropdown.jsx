f48bbc66c2fb207f8ad09b1ace411cb5638e7c64
Yashwitha <yashwitha@juegostudioz.com>
2022-11-22 09:55:29 +0530

    FEAT:Dropdown component is updated

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,44 +1,12 @@
-import React, { useState } from "react";
-
-import { getApiCall } from "../../services/apiServices";
-import { breedList, title, INITIAL_INDEX_VALUE } from "../../utils/constants";
-
 import styles from "./Dropdown.module.css";
 
-function DropDown({ setDropDownStatus, setBreedImageData }) {
-  const [DropdownTitle, setDropdownTitle] = useState(
-    title.defaultDropdownTitle
-  );
-  const [DropDownList, setDropDownList] = useState([]);
-  const [dropDownOpen, setDropDownOpen] = useState(false);
-
-  // OnClick of selectedItem in dropdown List
-  const onClickSelectedItemClicked = async (selectedItem) => {
-    setDropdownTitle(selectedItem);
-    setDropDownStatus(DropdownTitle);
-    setDropDownOpen((prevOpen) => !prevOpen);
-    setBreedImageData(selectedItem);
-  };
-  // set the drop down header title
-  const setDropdownData = async () => {
-    try {
-      const breedNameListData = await getApiCall(breedList);
-      if (breedNameListData) {
-        const breedNameList = Object.keys(breedNameListData).filter(
-          (key) => key.length > INITIAL_INDEX_VALUE
-        );
-        setDropDownList(breedNameList);
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
-  // onClick of Dropdown Header
-  const onClickDropdownHeader = async () => {
-    setDropDownOpen((prevOpen) => !prevOpen);
-    setDropdownData();
-  };
-
+function DropDown({
+  dropDownList,
+  onClickSelectedItemClicked,
+  onClickDropdownHeader,
+  dropDownTitle,
+  dropDownOpen,
+}) {
   return (
     <div className={styles.drop_down}>
       <div
@@ -47,11 +15,11 @@ function DropDown({ setDropDownStatus, setBreedImageData }) {
           onClickDropdownHeader();
         }}
       >
-        {DropdownTitle}
+        {dropDownTitle}
       </div>
       {dropDownOpen && (
         <div className={styles.content}>
-          {DropDownList.map((selectedItem, index) => {
+          {dropDownList.map((selectedItem, index) => {
             return (
               <div
                 key={index}
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,12 +4,22 @@ import ImageContainer from "../ImageContainer/ImageContainer";
 import ScrollingImageContainer from "../ScrollingImageContainer/ScrollingImageContainer";
 import { useState } from "react";
 import { getApiCall } from "../../services/apiServices";
-import { BreedImageList, INITIAL_INDEX_VALUE } from "../../utils/constants";
+import {
+  BreedImageList,
+  breedList,
+  INITIAL_INDEX_VALUE,
+  title,
+} from "../../utils/constants";
 
 function Gallery() {
   const [imageIndex, setImageIndex] = useState(0);
   const [dropDownStatusValue, setDropDownStatusValue] = useState();
   const [imageList, setImageList] = useState([]);
+  const [dropDownList, setDropDownList] = useState([]);
+  const [dropDownTitle, setDropdownTitle] = useState(
+    title.defaultDropdownTitle
+  );
+  const [dropDownOpen, setDropDownOpen] = useState(false);
 
   // Get the ImageIndex from the dropdown component functionality
   const getImageIndex = (indexValue) => {
@@ -20,6 +30,37 @@ function Gallery() {
   const setDropDownStatus = (dropDownStatus) => {
     setDropDownStatusValue(dropDownStatus);
   };
+
+  // set the drop down header title
+  const setDropdownData = async () => {
+    try {
+      const breedNameListData = await getApiCall(breedList);
+      if (breedNameListData) {
+        const breedNameList = Object.keys(breedNameListData).filter(
+          (key) => key.length > INITIAL_INDEX_VALUE
+        );
+        setDropDownList(breedNameList);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  // OnClick of selectedItem in dropdown List
+  const onClickSelectedItemClicked = async (selectedItem) => {
+    setDropdownTitle(selectedItem);
+    setDropDownStatus(dropDownTitle);
+    setDropDownOpen((prevOpen) => !prevOpen);
+    setBreedImageData(selectedItem);
+  };
+
+  // onClick of Dropdown Header
+  const dropdownHeader = async () => {
+    setDropDownOpen((prevOpen) => !prevOpen);
+    setDropdownData();
+    return dropDownTitle;
+  };
+
   // set Breed Image data
   const setBreedImageData = async (breedNameData) => {
     try {
@@ -39,8 +80,11 @@ function Gallery() {
   return (
     <div className={styles.gallery_container}>
       <DropDown
-        setDropDownStatus={setDropDownStatus}
-        setBreedImageData={setBreedImageData}
+        dropDownList={dropDownList}
+        onClickDropdownHeader={dropdownHeader}
+        dropDownTitle={dropDownTitle}
+        dropDownOpen={dropDownOpen}
+        onClickSelectedItemClicked={onClickSelectedItemClicked}
       />
       <ImageContainer imageList={imageList[imageIndex]} />
       <ScrollingImageContainer
